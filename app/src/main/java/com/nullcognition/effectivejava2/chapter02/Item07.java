36eb3b23efce40d5e1e705d652d9e19273da0762
ersin <ersin_the_ertan@hotmail.com>
2015-04-21 18:21:57 -0400

    item07 finalization - used for last effort logging of resource clean up, nondeterministic, not to be used for critical code (or most code)

--- app/src/main/java/com/nullcognition/effectivejava2/chapter02/Item07.java
+++ app/src/main/java/com/nullcognition/effectivejava2/chapter02/Item07.java
@@ -1,5 +1,7 @@
 package com.nullcognition.effectivejava2.chapter02;
 
+import android.util.Log;
+
 /**
  * Created by ersin on 18/04/15 at 8:02 PM
  */
@@ -12,5 +14,26 @@ package com.nullcognition.effectivejava2.chapter02;
 // provide explicity termination methods for files/threads in combiniation with try-finally
 // finalizers should log a warning if it finds that a resource has not been terminated
 
+public class Item07 {
 
-public class Item07 {}
+   String s = "hello world";
+
+   @Override
+   protected void finalize(){ // non deteriministic, can be accessed from from other threads thus keeping the object alive, but finalized
+
+	  try{
+		 super.finalize(); // when finalizing, always finalized the parent object
+	  }
+	  catch(Throwable inThrowable){inThrowable.printStackTrace();}
+	  if(s != null){ Log.e("Item07", "String not null, class did not terminateSelf");}
+   }
+
+   public void terminateSelf(){
+
+	  s = null;
+
+	  try{ finalize();}
+	  catch(Throwable inThrowable){ inThrowable.printStackTrace();}
+
+   }
+}
