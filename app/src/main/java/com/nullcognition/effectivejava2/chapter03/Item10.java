8e1abae424c0f0ff55824fc0497e1c5ac84126d1
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-05-05 21:51:44 -0400

    toString overriden to provide/or not a documented format but should not be used as the api, thus use getMethods() to return data presented in the output

--- app/src/main/java/com/nullcognition/effectivejava2/chapter03/Item10.java
+++ app/src/main/java/com/nullcognition/effectivejava2/chapter03/Item10.java
@@ -1,6 +1,41 @@
 package com.nullcognition.effectivejava2.chapter03;
 
+import java.util.Stack;
+
 /**
- * Created by ersin on 24/04/15 at 11:54 PM
+ Created by ersin on 24/04/15 at 11:54 PM
  */
-public class Item10 {}
+
+// always override toString
+// decide and document if format is immutable thus usable for parsing (as a protocol)
+// don't turn the output string to the api, provide access to data presented
+
+
+public class Item10{
+
+	// toSting
+
+	public class InformativeToString{
+		protected int i;
+		protected String s;
+		protected Stack<Integer> stack = new Stack<>();
+
+		{
+			stack.add(1);
+			stack.add(2);
+			stack.add(3);
+			stack.add(4);
+		}
+
+		public int getI(){ return i;}
+
+		public String getS(){ return s;}
+
+		public Integer getHead(){ return stack.peek();}
+
+		@Override
+		public String toString(){
+			return this.getClass().getSimpleName() + " i:" + i + " s:" + s + " head:" + stack.peek();
+		} // returns "Item10 i:3 s:helloWorld head:4"
+	}
+}
